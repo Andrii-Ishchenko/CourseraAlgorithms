15aba9f838347e62abe031b40d9fa307cc80bf70
Andrii Ishchenko <Andrii_Ishchenko1@epam.com>
2018-09-06 23:49:29 +0300

    initial commit with unionfind

---
+++ Main/UnionFind/IUnionFind.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.UnionFind
+{
+    interface IUnionFind
+    {
+        void Union(int p, int q);
+        bool IsConnected(int p, int q);
+    }
+}
---
+++ Main/UnionFind/UnionFind.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.UnionFind
+{
+    public class UnionFind : IUnionFind
+    {
+        private readonly int N;
+        private int[] id;
+
+        public UnionFind(int N)
+        {
+            this.N = N;
+            id = new int[N];
+            
+            for(int i = 0; i < N; i++)
+                id[i] = i;
+
+        }
+
+        public void Union(int p, int q)
+        {
+            int pid = id[p];
+            int qid = id[q];
+
+            for(int i=0; i<N; i++)
+                if( id[i] == pid) id[i] = qid;
+        }
+
+        public bool IsConnected(int p, int q)
+        {
+            return id[p] == id[q];
+        }
+    }
+}
